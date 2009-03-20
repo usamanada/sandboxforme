2f416b63717b32349e28dfa18e66eaa9a8555049
olofszymczak <olofszymczak@6a71ca32-14e2-11de-8b5c-13accb87f508>
2009-03-20 00:43:46 +0000

    Check In of SandBox.TimeDate

---
+++ SandBox.TimeDate/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SandBox.TimeDate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now.ToString("u"));
+
+            Console.WriteLine(now.ToString("zzzz"));
+            
+            Console.WriteLine(now.ToString("yyyyMMdd'T'HHmmss'.'FFF'Z'zzzz"));
+
+            Console.WriteLine(now.ToUniversalTime().ToString("yyyyMMdd'T'HHmmss'.'FFF'Z'"));
+
+        }
+    }
+}
