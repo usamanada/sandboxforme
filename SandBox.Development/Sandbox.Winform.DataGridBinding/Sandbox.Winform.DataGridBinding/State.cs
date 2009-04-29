6c8aab9b678f06c4e049d8c2f536f6cc63ebf437
olofszymczak <olofszymczak@6a71ca32-14e2-11de-8b5c-13accb87f508>
2009-04-29 23:48:43 +0000

    added winform.datagridbinding

---
+++ SandBox.Development/Sandbox.Winform.DataGridBinding/Sandbox.Winform.DataGridBinding/State.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace Sandbox.Winform.DataGridBinding
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
+    class State : Attribute
+    {
+        public string statename { get; set; }
+        public int id { get; set; }
+        
+    }
+}
